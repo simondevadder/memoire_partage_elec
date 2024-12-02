e143bebaec6d37bf6d3f97597c22764cec43cf8e
sdevadder <simon.devadder@student.uclouvain.be>
2024-12-02 09:20:24 +0100

    init and tests

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+Initialization file __init__.py
+"""
+
+from .energy_community import EnergyCommunity
+
---
+++ test/test_energy_community.py
@@ -0,0 +1,10 @@
+"""
+
+Master Thesis :  simulation of an energy community
+Master Student: Simon De Vadder
+ This file is used to test the functions in the file 'energy_community.py'
+"""
+
+import numpy as np
+from memoire_partage_elec.energy_community import EnergyCommunity
+
