2a402a88c363b06a412b22c3ddfeb09aaeb2f72d
sdevadder <simon.devadder@student.uclouvain.be>
2025-07-01 18:46:08 +0200

    fonctionne

--- new/opti.py
+++ new/opti.py
@@ -28,8 +28,10 @@ def first():
 
   # Affiche les 3 valeurs de la première ligne sous forme de floats
     n_households = load_profile.shape[1]  # à vérifier si pas 1
+    #n_households = 3  # Number of households, to change if needed
     dt = 0.25
     n_timestep = load_profile.shape[0]
+    #n_timestep = 96
     m.time = RangeSet(0,n_timestep-1)  # 35040 timesteps for one year with 15 minutes intervals
     m.households = RangeSet(0, n_households - 1)  # 1 household
     #print("Number of households:", n_households)
@@ -41,19 +43,19 @@ def first():
     #VARIABLES
     #########################
 
-    m.pv_area = Var(within=NonNegativeReals, bounds=(40, 2000), initialize=700)
-    m.wh_battery = Var(within=NonNegativeReals, initialize=10000)
+    m.pv_area = Var(within=NonNegativeReals, bounds=(40, 1000), initialize=700)
+    m.wh_battery = Var(within=NonNegativeReals, bounds=(0,50000), initialize=10000)
     #m.p_bat_max = Var(within=NonNegativeReals, initialize=2000)
     m.p = Var(m.households, m.time, within=NonNegativeReals, initialize=0)  # Power to each household in W
     m.p_bat_pos = Var(m.time, within=NonNegativeReals, initialize=0)
     m.p_bat_neg = Var(m.time, within=NonNegativeReals, initialize=0)  # Power to the battery in W
-    m.p_inj = Var(m.time, within=NonNegativeReals, initialize=0)
+    #m.p_inj = Var(m.time, within=NonNegativeReals, initialize=0)
     m.p_ev = Var(m.time, within=NonNegativeReals, initialize=0)  # Power to EV, not used in this model
-    m.soc = Var(m.time, within=NonNegativeReals, initialize=0)  # State of charge of the battery in Wh, not used in this model
-    m.p_pv = Var(m.time, within=NonNegativeReals, initialize=0)  # Power from PV in W, not used in this model
-    m.kWc = Var(within=NonNegativeReals, initialize=127.4)
-    m.cv_coeff = Var(within=NonNegativeReals, bounds=(0.0, 2.1), initialize=1)  # Coefficient for the piecewise function, bounds can be adjusted
-    m.price_per_kwc = Var(within=NonNegativeReals, bounds=(0, 4000), initialize=1000)  # Price per kWc, bounds can be adjusted
+    m.soc = Var(m.time, within=NonNegativeReals, initialize=5000)  # State of charge of the battery in Wh, not used in this model
+    #m.p_pv = Var(m.time, within=NonNegativeReals, initialize=0)  # Power from PV in W, not used in this model
+    m.kWc = Var(within=NonNegativeReals, bounds=(7.27, 365), initialize=127.4)
+    m.cv_coeff = Var(within=NonNegativeReals, bounds=(0.58, 1.953), initialize=1)  # Coefficient for the piecewise function, bounds can be adjusted
+    m.price_per_kwc = Var(within=NonNegativeReals, bounds=(0, 4000), initialize=1500)  # Price per kWc, bounds can be adjusted
     m.p_ev_max = Var(within=NonNegativeReals, initialize=74000)  # Maximum power of the battery in W, to change as a ffunction to the capacity
 
     #########################
@@ -65,17 +67,35 @@ def first():
     m.annual_rate = Param(initialize=0.03)  # Annual rate for the PV costs
     m.lifetime = Param(initialize=25)  # Lifetime of the PV system in years
     year = 0
-    breakpoints = [0, 5, 36, 100, 250, 10000] # for cv_coeff
+    breakpoints = [7.27, 36, 100, 250, 365] # for cv_coeff
     
-    breakpoints_price = [0, 10, 50, 100, 10000]  # adapte la borne supérieure si besoin
+    breakpoints_price = [0, 10, 50, 100, 100000]  # adapte la borne supérieure si besoin
 
     # Les coefficients (x, y) pour chaque intervalle
     # Pour chaque palier : price_per_kwc = x * kWc + y
     price_x = [-240, -3.75, -3, -0.66]
     price_y = [3700, 1337.5, 1300, 1066.6]
     
+    # print("Min production_profile:", np.min(production_profile[:, year]))
+    # print("Max production_profile:", np.max(production_profile[:, year]))
+    # print("Any negative?", np.any(production_profile[:, year] < 0))
+    # print("production_profile[35009, year] =", production_profile[35009, year])
+    production_profile = np.maximum(production_profile, 0)
     
+    def pv_prod_rule(m, t):
+        return production_profile[t, year] * m.pv_area
+    m.pv_prod = Expression(m.time, rule=pv_prod_rule)
     
+    def p_expr_rule(m, h, t):
+        total_load = sum(load_profile[t, hh] for hh in range(n_households))
+        pv_prod = m.pv_prod[t] + m.p_bat_neg[t]
+        if total_load == 0:
+            return 0
+        else:
+            return load_profile[t, h] * pv_prod / total_load     
+    m.p_expr = Expression(m.households, m.time, rule=p_expr_rule)
+
+
     #########################
     #OBJECTIVE
     #########################
@@ -99,15 +119,18 @@ def first():
         price_day = 0.38
         price_night = 0.29
         price_injection = 0.04  # price for injection, not used in this model
-        price_ev = 0.2
+        price_ev = -1
         #####################
         #Revenue
         #####################
         total_bill_reduction = sum(
-        m.p[h, t] * (price_night if time_is_night(t) else price_day) * dt * 0.001
+        m.p[h,t] * (price_night if time_is_night(t) else price_day) * dt * 0.001
         for t in m.time for h in m.households
         )
-        total_from_injection = sum(m.p_inj[t] * price_injection * dt * 0.001 for t in m.time)
+        p_inj = sum(m.pv_prod[t] - (
+            sum(m.p[h, t] for h in m.households) + m.p_bat_pos[t] + m.p_ev[t] - m.p_bat_neg[t]
+        ) for t in m.time)
+        total_from_injection = p_inj * price_injection * dt * 0.001 
         total_from_ev = sum(m.p_ev[t] * price_ev * dt * 0.001 for t in m.time)
             
         #print("total_bill_reduction:", total_bill_reduction)
@@ -117,8 +140,8 @@ def first():
         #################
         # CV
         ##################
-        
-        cv_revenue = sum(m.p_pv[t] for t in m.time) * m.cv_coeff * 65 * dt * 10 /(25 * 1000000)        #print("cv_revenue:", cv_revenue)
+
+        cv_revenue = sum(m.pv_prod[t] for t in m.time) * m.cv_coeff * 65 * dt * 10 /(25 * 1000000)        #print("cv_revenue:", cv_revenue)
         #print("cv_revenue:", cv_revenue)
         ###############
         # PV Costs
@@ -152,13 +175,28 @@ def first():
     #CONSTRAINTS
     #########################
     
+    def p_rule_1(m, h, t):
+        return m.p[h, t] <= m.p_expr[h, t]
+    m.p_rule_1 = Constraint(m.households, m.time, rule=p_rule_1)
+    
+    def p_rule_2(m, h, t):
+        return m.p[h, t] <= load_profile[t, h]
+    m.p_rule_2 = Constraint(m.households, m.time, rule=p_rule_2)
+    
     def kwc_def_rule(m):   ## for cv_coeff
        return m.kWc == m.pv_area * 0.182
+   
+    # def cv_unique_rule(m):
+    #     return m.cv_coeff == 2 # Coefficient for the piecewise function, can be adjusted
+    # m.cv_unique = Constraint(rule=cv_unique_rule)
+    
+    # def price_unique_rule(m):
+    #     return m.price_per_kwc == 1500
+    # m.price_unique = Constraint(rule=price_unique_rule)
+    
     m.kwc_def = Constraint(rule=kwc_def_rule)
     def cv_piecewise_rule(m, x):
-        if x < 5:
-            return 2.055
-        elif x < 36:
+        if x < 36:
             return 1.953
         elif x < 100:
             return 1.016
@@ -168,7 +206,7 @@ def first():
             return 0.58
         #return 0
     m.piecewise_cv = Piecewise(
-    m.kWc, m.cv_coeff,
+    m.cv_coeff, m.kWc,
     pw_pts=breakpoints,
     f_rule=cv_piecewise_rule,
     pw_constr_type='EQ',
@@ -186,20 +224,17 @@ def first():
             return -0.66 * x + 1066.6
 
     m.piecewise_price = Piecewise(
-        m.kWc, m.price_per_kwc,
+        m.price_per_kwc, m.kWc,
         pw_pts=breakpoints_price,
         f_rule=price_piecewise_rule,
         pw_constr_type='EQ',
         pw_repn='SOS2'
     )
     
-    def power_to_household(m, h, t):
-        return m.p[h, t] <= load_profile[t, h]
-    m.power_to_household = Constraint(m.households, m.time, rule=power_to_household)
-    
-    def power_from_pv(m, t):
-        return m.p_pv[t] == production_profile[t, year] * m.pv_area
-    m.power_from_pv = Constraint(m.time, rule=power_from_pv)
+
+    # def power_from_pv(m, t):
+    #     return m.p_pv[t] == production_profile[t, year] * m.pv_area
+    # m.power_from_pv = Constraint(m.time, rule=power_from_pv)
     
     def power_to_battery_pos(m, t):
         return m.p_bat_pos[t] <= m.p_bat_max
@@ -212,12 +247,17 @@ def first():
     # def power_bat_limit(m):
     #     return m.p_bat_max <= m.wh_battery * 0.5
     # m.power_bat_limit = Constraint(rule=power_bat_limit)
-    
-    def power_equality(m,t):
-        return  m.p_inj[t] + m.p_bat_pos[t] - m.p_bat_neg[t] + sum(m.p[h, t] for h in m.households) + m.p_ev[t] == m.p_pv[t]
-    m.power_equality = Constraint(m.time, rule=power_equality)
-    
-    def soc_set(m,t):
+    def total_consumption_limit(m, t):
+        return sum(m.p[h,t] for h in m.households) + m.p_bat_pos[t] + m.p_ev[t] <= production_profile[t, year] * m.pv_area + m.p_bat_neg[t]
+    m.total_consumption_limit = Constraint(m.time, rule=total_consumption_limit)
+
+    # def power_equality(m, t):
+    #     return m.p_inj[t] == production_profile[t, year] * m.pv_area - (
+    #         sum(m.p[h, t] for h in m.households) + m.p_bat_pos[t] + m.p_ev[t] - m.p_bat_neg[t]
+    #     )
+    # m.power_equality = Constraint(m.time, rule=power_equality)
+
+    def soc_set(m, t):
         if t == 0:
             return m.soc[t] == 0.5 *m.wh_battery  # Initial state of charge at 50% of the battery capacity
         else:
@@ -226,7 +266,7 @@ def first():
     
     def soc_lower_limit(m, t):
         return m.soc[t] >= 0.2 * m.wh_battery  # Minimum state of charge at 20% of the battery capacity
-    m.soc_lower_limit = Constraint(m.time, rule=soc_lower_limit)
+    #m.soc_lower_limit = Constraint(m.time, rule=soc_lower_limit)
     
     def soc_upper_limit(m, t):
         return m.soc[t] <= 0.9 * m.wh_battery  # Maximum state of charge at 90% of the battery capacity
@@ -244,38 +284,64 @@ def first():
         return m.p_ev[t] <= m.p_ev_max
     m.p_ev_limit = Constraint(m.time, rule=p_ev_limit)
     
-    
+        # Avant le modèle Pyomo, calcule la demande totale pour chaque t
+    total_load = {t: sum(load_profile[t, h] for h in range(n_households)) for t in range(n_timestep)}
+
+    # def repartition_rule1(m, h, t):
+    #     if total_load[t] == 0:
+    #         return m.p[h, t] == 0
+    #     return m.p[h, t] <= load_profile[t, h] * production_profile[t, year] * m.pv_area / total_load[t]
+    # m.repartition1 = Constraint(m.households, m.time, rule=repartition_rule1)
+
+    # def repartition_rule2(m, h, t):
+    #     return m.p[h, t] <= load_profile[t, h]
+    # m.repartition2 = Constraint(m.households, m.time, rule=repartition_rule2)
+
     gurobi.solve(m)
-    puissance_totale = sum(value(m.p[h, t]) for h in m.households for t in m.time)*0.25
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    from pyomo.util.infeasible import log_infeasible_constraints
+    log_infeasible_constraints(m)
+    
+    puissance_totale = sum(value(m.p[h,t]) for h in m.households for t in m.time)*0.25
+    
+    p_inj = sum(production_profile[t, year] * value(m.pv_area) - (
+                sum(value(m.p[h, t]) for h in m.households) + value(m.p_bat_pos[t]) + value(m.p_ev[t]) - value(m.p_bat_neg[t])
+            ) for t in m.time)
     print("Puissance totale distribuée aux ménages :", puissance_totale, "Wh")
-    print("puissance injectée :", sum(value(m.p_inj[t]) for t in m.time)*.025, "Wh")
-    print("puissance produite par le PV (p_pv) :", sum(value(m.p_pv[t])*0.25 for t in m.time))
+    print("puissance injectée :", p_inj * 0.025, "Wh")
+    print("puissance produite par les PV (p_pv) :", sum(production_profile[t, year] * value(m.pv_area)*0.25 for t in m.time))
 
 
      # Affichage des résultats principaux
     print("Objectif (revenu total) :", value(m.objective))
     print("Surface PV (pv_area) :", value(m.pv_area))
+
+    print("production_profile[65, year] :", production_profile[65, year])
+    
     print("Capacité batterie (wh_battery) :", value(m.wh_battery))
     print("Puissance batterie max (p_bat_max) :", value(m.p_bat_max))
     print("cv_coeff :", value(m.cv_coeff))
-    print("price", value(m.price_per_kwc))  # Affichage du prix par kWc
+    print("price par kwc", value(m.price_per_kwc))  # Affichage du prix par kWc
     pv_costs = value(m.kWc) * value(m.price_per_kwc)  # Calcul du coût total du PV
     pv_costs_annualized = pv_costs * (m.annual_rate / (1 - (1 + m.annual_rate) ** -m.lifetime))  # 25 years lifetime
     print("pv costs", pv_costs_annualized)
 
-    cv_revenue = sum(value(m.p_pv[t]) for t in m.time) * value(m.cv_coeff) * 65 * dt * 10 /(25 * 1000000)
+    cv_revenue = sum(production_profile[t, year] * value(m.pv_area) for t in m.time) * value(m.cv_coeff) * 65 * dt * 10 /(25 * 1000000)
     print("cv_revenue:", cv_revenue)
     price_day = 0.38
     price_night = 0.29
     price_injection = 0.04  # price for injection, not used in this model
     total_bill_reduction = sum(
-        value(m.p[h, t]) * (price_night if time_is_night(t) else price_day) * dt * 0.001
+        value(m.p_expr[h, t]) * (price_night if time_is_night(t) else price_day) * dt * 0.001
         for t in m.time for h in m.households
         )
     print("total_bill_reduction:", total_bill_reduction)
-    total_from_injection = sum(value(m.p_inj[t]) * price_injection * dt * 0.001 for t in m.time)
+
+    total_from_injection = p_inj * price_injection * dt * 0.001   
     print("total_from_injection:", total_from_injection)
     print("ev_max:", value(m.p_ev_max))
+    
     
 
 first()
